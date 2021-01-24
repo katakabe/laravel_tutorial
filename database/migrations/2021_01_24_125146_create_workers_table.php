b5f4e234a860be2c5d05f6056f516f503fcc2cf9
katakabe <katakabe.shi@gmail.com>
2021-01-24 23:32:13 +0900

    入力フォームからDB挿入の流れ

---
+++ database/migrations/2021_01_24_125146_create_workers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWorkersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // workers テーブルを作成する
+        Schema::create('workers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('username')->comment('名前');
+            $table->string('mail')->comment('メール');
+            $table->integer('age')->comment('年齢');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('workers');
+    }
+}
